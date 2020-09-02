82d195c63289e6f3aa11e6ae355ea89d1a1e4de4
Shubham <shiroresn@gmail.com>
2020-09-02 23:34:57 +0530

    required models created

---
+++ Test/Test/ClientApp/src/app/models/employee.ts
@@ -0,0 +1,11 @@
+export class Employee {
+    id:number;
+    employeeName:string;
+    email:string;
+    contactNumber:string;
+    gender:string;
+    departmentId:number;
+    uploadedPhoto:string;
+    areaOfInterest:any;
+    isActive:any;
+}
---
+++ Test/Test/ClientApp/src/app/models/interest.ts
@@ -0,0 +1,4 @@
+export  class Interest{
+    id:number;
+    name:string;
+}
